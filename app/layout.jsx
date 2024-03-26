7994e3fb08952b80a1c4d4022bc09442dcd69fcd
Tim Traylor <ttraylor310@gmail.com>
2024-03-26 15:39:31 -0700

    added assets with navbar component. check text before continue. links and structure to come

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,12 +1,16 @@
 // import { Inter } from "next/font/google";
 // const inter = Inter({ subsets: ["latin"] });
+import Navbar from '@/components/Navbar'
+import '@/assets/styles/global.css'
 
 export const metadata = {
-  title: 'TrayTestingApp',
+  title: 'TrayTestingApp change tab title',
+  // need to fine tune the description
   description: 'thingsto test',
+  keywords: 'test, test, test',
 }
 
-export default function RootLayout({ children }) {
+export default function MainLayout({ children }) {
   return (
     <html lang='en'>
       {/* <body className={inter.className}> */}
